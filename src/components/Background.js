cd53eca8bab22a52f6c0f80883a05d209c9a6ca7
cammillen <cameronrmillen@gmail.com>
2023-02-17 15:03:24 +0000

    adding components

---
+++ src/components/Background.js
@@ -0,0 +1,2 @@
+// This is the standard background with the squares on it.
+// There also needs to be another part which is the black background underneath it that allows for the scrollable behaviour. 
