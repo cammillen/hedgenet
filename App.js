1be71e99022e8c628e64a0e83a71c07e80376501
cammillen <cameronrmillen@gmail.com>
2023-02-23 15:55:58 +0000

    changes

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import BottomTabsNavigator from './navigation/BottomTabsNavigator';
+import BottomTabsNavigator from './src/navigation/BottomTabsNavigator';
 
 const App = () => {
     return(
