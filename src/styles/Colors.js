5553afb06915ab125cf93f8035cb433eb33ef812
ArranTClark <arran.clark.22@ucl.ac.uk>
2023-02-24 14:48:10 +0000

    beginning of colour definitions

--- src/styles/Colors.js
+++ src/styles/Colors.js
@@ -1,1 +1,36 @@
 // This is where we lay out the colors
+
+const colours = StyleSheet.create({
+    primaryGreen: {
+        colour: '#12D18E',
+        //also success
+    },
+    secondaryYellow: {
+        colour: '#FFC107',
+        //also warning
+    },
+    infoBlue: {
+        colour: '#6949FF',
+    },
+    errorRed: {
+        colour: '#F75555',
+    },
+    disabledGrey: {
+        colour: '#D8D8D8',
+    },
+    disabledGreen: {
+        colour: '#0EA772',
+    },
+    greyscale: {
+        _900: {},
+        _800: {},
+        _700: {},
+        _600: {},
+        _500: {},
+        _400: {},
+        _300: {},
+        _200: {},
+        _100: {},
+        _50: {},
+    },
+  });
