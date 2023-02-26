a765003447325cdf6625dd586856cb86ec3c7dcf
ArranTClark <arran.clark.22@ucl.ac.uk>
2023-02-26 20:05:55 +0000

    added colours' hex; not gradiation yet

--- src/styles/Colors.js
+++ src/styles/Colors.js
@@ -1,36 +1,82 @@
 // This is where we lay out the colors
 
+
 const colours = StyleSheet.create({
-    primaryGreen: {
-        colour: '#12D18E',
-        //also success
+
+    main: {
+        green: {colour: '#12D18E'},
+        yellow: {colour: '#FFC107'}
     },
-    secondaryYellow: {
-        colour: '#FFC107',
-        //also warning
+
+    status: {
+        green: {colour: '#12D18E'},//repeat of above
+        disGreen: {colour: '#0EA772'},
+        yellow: {colour: '#FFC107'},//repeat of above
+        blue: {colour: '#6949FF'},
+        grey: {colour: '#D8D8D8'},
+        red: {colour: '#F75555'}
     },
-    infoBlue: {
-        colour: '#6949FF',
-    },
-    errorRed: {
-        colour: '#F75555',
-    },
-    disabledGrey: {
-        colour: '#D8D8D8',
-    },
-    disabledGreen: {
-        colour: '#0EA772',
-    },
+    
     greyscale: {
-        _900: {},
-        _800: {},
-        _700: {},
-        _600: {},
-        _500: {},
-        _400: {},
-        _300: {},
-        _200: {},
-        _100: {},
-        _50: {},
+        _900: {colour: '#212121'},
+        _800: {colour: '#424242'},
+        _700: {colour: '#616161'},
+        _600: {colour: '#757575'},
+        _500: {colour: '#9E9E9E'},
+        _400: {colour: '#BDBDBD'},
+        _300: {colour: '#E0E0E0'},
+        _200: {colour: '#EEEEEE'},
+        _100: {colour: '#F5F5F5'},
+        _50: {colour: '#FAFAFA'}
     },
+
+    dark: {
+        _1: {'#181A20'},
+        _2: {'#1F222A'},
+        _3: {'#35383F'}
+    },
+
+    transparent: {
+        green: {colour: '#12D18E'},
+        teal: {colour: '#019B83'},
+        yellow: {colour: '#FFD300'},
+        blue: {colour: '#246BFD'},
+        purple: {colour: '#6949FF'},
+        red: {colour: '#FF5A5F'},
+        orange: {colour: '#FF9800'},
+        cyan: {colour: '#00BCD4'}
+    },
+
+    background: {
+        green: {colour: '#F1FDF5'},
+        teal: {colour: '#F2FFFD'},
+        yellow: {colour: '#FFFCEB'},
+        blue: {colour: '#F6F9FF'},
+        purple: {colour: '#F9F8FF'},
+        red: {colour: '#FFF7F8'},
+        orange: {colour: '#FFF8ED'}
+    },
+
+    other: {
+        white: {colour: '#FFFFFF'},
+        black: {colour: '#000000'},
+        red: {colour: '#F44336'},
+        pink: {colour: '#E91E63'},
+        purple: {colour: '#9C27B0'},
+        deepPurple: {colour: '#673AB7'},
+        indigo: {colour: '#3F51B5'},
+        blue: {colour: '#2196F3'},
+        lightBlue: {colour: '#03A9F4'},
+        cyan: {colour: '#00BCD4'},
+        teal: {colour: '#009688'},
+        green: {colour: '#4CAF50'},
+        lightGreen: {colour: '#8BC34A'},
+        lime: {colour: '#CDDC39'},
+        yellow: {colour: '#FFEB3B'},
+        amber: {colour: '#FFC107'},
+        orange: {colour: '#FF9800'},
+        deepOrange: {colour: '#FF5722'},
+        brown: {colour: '#795548'},
+        blueGrey: {colour: '#607D8B'}
+    }
   });
