874042107f1f1685ee39297dcc99eefae4ec709a
cammillen <cameronrmillen@gmail.com>
2023-02-28 19:31:03 +0000

    changes background and top menu bar

--- App.js
+++ App.js
@@ -7,6 +7,7 @@ import { useFonts } from 'expo-font';
 import * as SplashScreen from 'expo-splash-screen';
 import { globalColors } from './src/styles/Colors.js';
 import { globalFonts } from './src/styles/Fonts.js';
+import TopMenuBar from './src/components/TopMenuBar.js';
 
 // Loading the custom fonts (you have to use this at the start of every screen): 
 
@@ -34,6 +35,7 @@ export default function App() {
 
   return (
     <View style={styles.container} onLayout={onLayoutRootView}>
+      <TopMenuBar />
       <Text style={globalFonts.BodyXLarge.Regular(globalColors.primary._500.color)}>Urbanist Bold</Text>
       <Text style={globalFonts.BodyLarge.Regular(globalColors.primary._400.color)}>Urbanist Bold</Text>
       <Text style={globalFonts.BodyMedium.Regular(globalColors.primary._300.color)}>Urbanist Bold</Text>
@@ -49,8 +51,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 50,
   },
 });
--- src/components/TopMenuBar.js
+++ src/components/TopMenuBar.js
@@ -1,2 +1,60 @@
 // This is the top menu bar that contains all the top navigation componenets.
 // It needs to include the black background behind the menu bar (see figma file).
+import React from 'react';
+import { Image } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
+import { globalColors } from '../styles/Colors.js';
+import { globalFonts } from '../styles/Fonts.js';
+
+function TopMenuBar() {
+    return (
+      <View style={styles.header}>
+        <View style={styles.iconContainer}>
+          <Image
+            source={require('../assets/icons/Avatar.png')}
+            style={[styles.avatarIcon, { marginRight: 19 }]}
+          />
+          <Image
+            source={require('../assets/icons/Notification.png')}
+            style={[styles.notificationIcon, { marginRight: 19 }]}
+          />
+        </View>
+        <Text style={globalFonts.H4(globalColors.others.white.color)}>Home</Text>
+        <View style={styles.iconContainer}>
+          <Image
+            source={require('../assets/icons/Chart.png')}
+          />
+          <Image
+            source={require('../assets/icons/Chart.png')}
+            style={{ marginLeft: 19 }}
+          />
+        </View>
+      </View>
+    );
+  }  
+
+const styles = StyleSheet.create({
+    header: {
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      alignItems: 'center',
+      backgroundColor: globalColors.dark._1.color,
+      padding: 16,
+    },
+    iconContainer: {
+      flexDirection: 'row',
+      alignItems: 'center',
+    },
+    avatarIcon: {
+        width: 33,
+        height: 33,
+        resizeMode: 'contain',
+    },
+    notificationIcon: {
+        width: 21.08,
+        height: 24,
+        resizeMode: 'contain',
+    },
+  });
+  
+  export default TopMenuBar;
