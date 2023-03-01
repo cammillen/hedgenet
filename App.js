7f7e602ee4a836e77bb2a0b880673ca8f939b5f9
cammillen <cameronrmillen@gmail.com>
2023-03-01 14:30:35 +0000

    small changes

--- App.js
+++ App.js
@@ -2,6 +2,7 @@
 
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import { StatusBar } from 'react-native';
 import { useCallback } from 'react';
 import { useFonts } from 'expo-font';
 import * as SplashScreen from 'expo-splash-screen';
@@ -34,15 +35,19 @@ export default function App() {
 // Custom fonts loaded, code below is for the screen: 
 
   return (
-    <View style={styles.container} onLayout={onLayoutRootView}>
-      <TopMenuBar />
-      <Text style={globalFonts.BodyXLarge.Regular(globalColors.primary._500.color)}>Urbanist Bold</Text>
-      <Text style={globalFonts.BodyLarge.Regular(globalColors.primary._400.color)}>Urbanist Bold</Text>
-      <Text style={globalFonts.BodyMedium.Regular(globalColors.primary._300.color)}>Urbanist Bold</Text>
-      <Text style={globalFonts.BodySmall.Regular(globalColors.primary._200.color)}>Urbanist Bold</Text>
-      <Text style={globalFonts.BodyXSmall.Regular(globalColors.primary._100.color)}>Urbanist Bold</Text>
-      <Text style={{ fontSize: 30, color: globalColors.greyscale._900.color }}>Platform Default</Text>
-    </View>
+    <>
+      <StatusBar barStyle="light-content" backgroundColor="#000" />
+      <View style={styles.container} onLayout={onLayoutRootView}>
+        <TopMenuBar />
+        <Text></Text>
+        <Text style={globalFonts.BodyXLarge.Regular(globalColors.primary._500.color)}>Urbanist Bold</Text>
+        <Text style={globalFonts.BodyLarge.Regular(globalColors.primary._400.color)}>Urbanist Bold</Text>
+        <Text style={globalFonts.BodyMedium.Regular(globalColors.primary._300.color)}>Urbanist Bold</Text>
+        <Text style={globalFonts.BodySmall.Regular(globalColors.primary._200.color)}>Urbanist Bold</Text>
+        <Text style={globalFonts.BodyXSmall.Regular(globalColors.primary._100.color)}>Urbanist Bold</Text>
+        <Text style={{ fontSize: 30, color: globalColors.greyscale._900.color }}>Platform Default</Text>
+      </View>
+    </>
   );
 }
 
