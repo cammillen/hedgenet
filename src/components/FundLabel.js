b23eea8a3a3b168f005d10666d7a356501195713
cammillen <cameronrmillen@gmail.com>
2023-03-02 16:27:31 +0000

    value card

--- src/components/FundLabel.js
+++ src/components/FundLabel.js
@@ -6,6 +6,19 @@ import { StyleSheet, Text, View } from 'react-native';
 import { globalColors } from '../styles/Colors.js';
 import { globalFonts } from '../styles/Fonts.js';
 
+export default function FundLabel(param) {
+
+  return (
+    <View style={styles.header}>
+      <View style={styles.subHeader}>
+        <Image source={require('../assets/icons/HedgenetWhite.png')} style={[styles.hedgenetIcon, { marginRight: 16, marginLeft: 24}]} />
+        <Text style={globalFonts.H4(globalColors.others.white.color)}>{param.name}</Text>
+        <Text style={globalFonts.H4(globalColors.others.white.color)}></Text>
+      </View>
+      <Image source={require('../assets/icons/MoreIcon.png')} style={[styles.moreIcon, { marginRight: 24 }]} />
+    </View>
+  );
+}
 
 const styles = StyleSheet.create({
   header: {
@@ -23,8 +36,8 @@ const styles = StyleSheet.create({
     maxHeight: 30
   },
   hedgenetIcon: {
-    width: 20.4,
-    height: 24.27,
+    width: 28,
+    height: 28,
     resizeMode: 'contain',
   },
   moreIcon: {
@@ -33,18 +46,4 @@ const styles = StyleSheet.create({
     resizeMode: 'contain'
 }
 });
-
-export default function FundLabel(param) {
-
-  return (
-    <View style={styles.header}>
-      <View style={styles.subHeader}>
-        <Image source={require('../assets/icons/HedgenetWhite.png')} style={[styles.hedgenetIcon, { marginRight: 16, marginLeft: 24}]} />
-        <Text style={globalFonts.H4(globalColors.others.white.color)}>{param.name}</Text>
-        <Text style={globalFonts.H4(globalColors.others.white.color)}>...</Text>
-      </View>
-      <Image source={require('../assets/icons/MoreIcon.png')} style={[styles.moreIcon, { marginRight: 24 }]} />
-    </View>
-  );
-}
 
