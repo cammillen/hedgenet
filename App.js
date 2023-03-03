f542b9c6c3d3d1add2e7c23c106e3b424e1895bc
cammillen <cameronrmillen@gmail.com>
2023-03-03 11:13:58 +0000

    background push

--- App.js
+++ App.js
@@ -15,8 +15,9 @@ import MyPositions from './src/components/MyPositions.js';
 import PositionsLoop from './src/components/PositionsLoop.js';
 import SliderBar from './src/components/SliderBar.js';
 import { NavigationContainer } from '@react-navigation/native';
-import SearchBarInactive from './src/components/SearchBarInactive.js'
-import SearchBarActive from './src/components/SearchBarActive.js'
+import SearchBarInactive from './src/components/SearchBarInactive.js';
+import SearchBarActive from './src/components/SearchBarActive.js';
+import Background from './src/components/Background.js';
 
 // Loading the custom fonts (you have to use this at the start of every screen): 
 
@@ -43,19 +44,21 @@ export default function App() {
   // Custom fonts loaded, code below is for the screen: 
   return (
     <NavigationContainer>
-    <>
-      <StatusBar barStyle="light-content" backgroundColor="#000" />
-      <View style={styles.container} onLayout={onLayoutRootView}>
-        <TopMenuBar />
-        <SearchBarInactive/>
-        <SearchBarActive/>
-        <FundLabel name={fundName} />      
-        <ValueCard cashBalance={cash} delta={difference} />
-        <SliderBar />
-        <MyPositions />
-        <PositionsLoop stocks={['Google','Netflix']} />
-      </View>
-    </>
+      <Background>
+      <>
+        <StatusBar barStyle="light-content" backgroundColor="#000" />
+        <View style={styles.container} onLayout={onLayoutRootView}>
+          <TopMenuBar />
+          <SearchBarInactive/>
+          <SearchBarActive/>
+          <FundLabel name={fundName} />      
+          <ValueCard cashBalance={cash} delta={difference} />
+          <SliderBar />
+          <MyPositions />
+          <PositionsLoop stocks={['Google','Netflix']} />
+        </View>
+      </>
+      </Background>
     </NavigationContainer>
   );
 }
--- src/components/Background.js
+++ src/components/Background.js
@@ -1,2 +1,27 @@
 // This is the standard background with the squares on it.
 // There also needs to be another part which is the black background underneath it that allows for the scrollable behaviour. 
+
+import React from 'react';
+import { View, ImageBackground, StyleSheet } from 'react-native';
+
+const backgroundImage = require('../assets/background.png');
+
+export default function Background(props) {
+  return (
+    <View style={styles.container}>
+      <ImageBackground source={backgroundImage} style={styles.backgroundImage}>
+        {props.children}
+      </ImageBackground>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  backgroundImage: {
+    flex: 1,
+    resizeMode: 'cover',
+  },
+});
