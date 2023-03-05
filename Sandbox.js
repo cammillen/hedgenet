1ff06cd85f53e155d4b14dfcd7693c9cdf8bcd21
omirostr1 <115593155+omirostr1@users.noreply.github.com>
2023-03-05 17:45:20 +0000

    Revert "Merge branch 'dev0.2' of https://github.com/cammillen/hedgenet into dev0.2"
    This reverts commit 65fbe44fae26a93287af6e57b9cf978e49ca8887, reversing
    changes made to 2f30d24977f430eafee6b8a84f91ccbd2f918aa9.

--- Sandbox.js
+++ Sandbox.js
@@ -1,30 +1,75 @@
-import * as React from "react"
-import {View} from 'react-native'
-import Svg, { G, Path, Defs } from "react-native-svg"
-import { BlurView } from 'expo-blur';
-/* SVGR has dropped some elements not supported by react-native-svg: filter */
+// This is the top menu bar that contains all the top navigation componenets.
+// It needs to include the black background behind the menu bar (see figma file).
+import React from 'react';
+import { Image } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
+import { globalColors } from '../styles/Colors.js';
+import { globalFonts } from '../styles/Fonts.js';
+import { useNavigation } from '@react-navigation/native';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 
-const SvgComponent = (props) => (
+function TopMenuBar() {
+    const navigation = useNavigation();
+    
+    return (
+      <View style={styles.header}>
+        <View style={styles.iconContainer}>
+          <Image
+            source={require('../assets/icons/Avatar.png')} // BACKEND TO DO: backend integration with profile picture. 
+            style={[styles.avatarIcon, { marginRight: 19 },  { marginLeft: 24 }]}
+          />
+          <Image
+            source={require('../assets/icons/Notification.png')}
+            style={styles.notificationIcon}
+          />
+        </View>
+        <Text style={globalFonts.H4(globalColors.others.white.color)}>Link</Text> 
+        <View style={styles.iconContainer}>
+          <Image
+            source={require('../assets/icons/Star.png')}
+            style={styles.starIcon}
+          />
+          <Image
+            source={require('../assets/icons/Message.png')}
+            style={[styles.messageIcon, { marginLeft: 26 }, { marginRight: 24 }]}
+          />
+        </View>
+      </View>
+    );
+  }  
 
-  <Svg
-    width={381}
-    height={113}
-    fill="none"
-    xmlns="http://www.w3.org/2000/svg"
-    {...props}
-    backgroundColor='transparent'
-  >
-    <G filter="url(#a)">
-      <Path
-        fillRule="evenodd"
-        clipRule="evenodd"
-        d="M124.11 31.993c11.176.002 20.643-7.73 28.216-15.945C161.379 6.228 174.893-.002 189.993 0c15.107.002 28.633 6.243 37.696 16.072 7.573 8.214 17.039 15.94 28.209 15.941l98.111.015c14.356.002 25.998 11.64 26.005 25.995l.016 28.972c.008 14.363-11.632 26.007-25.995 26.005l-327.994-.05C11.685 112.948.043 101.311.036 86.955L.02 57.983C.012 43.62 11.652 31.976 26.015 31.978l98.095.015Z"
-        fill="#fff"
-        fillOpacity={0.1}
-      />
-    </G>
-    <Defs></Defs>
-  </Svg>
-)
-
-export default SvgComponent
+const styles = StyleSheet.create({
+    header: {
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      alignItems: 'center',
+      backgroundColor: globalColors.others.black.color,
+      padding: 10,
+    },
+    iconContainer: {
+      flexDirection: 'row',
+      alignItems: 'center',
+    },
+    avatarIcon: {
+        width: 33,
+        height: 33,
+        resizeMode: 'contain',
+    },
+    notificationIcon: {
+        width: 21.08,
+        height: 24.5,
+        resizeMode: 'contain',
+    },
+    starIcon: {
+        width: 24.51,
+        height: 23.25,
+        resizeMode: 'contain',
+    },
+    messageIcon: {
+        width: 26,
+        height: 23.58,
+        resizeMode: 'contain',
+    },
+  });
+  
+  export default TopMenuBar;
