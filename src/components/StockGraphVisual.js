d0a50c431bab8053ef3a8465f34502dc4fb41d51
ArranTClark <arran.clark.22@ucl.ac.uk>
2023-03-06 22:24:59 +0000

    graph trial for Arei

--- src/components/StockGraphVisual.js
+++ src/components/StockGraphVisual.js
@@ -13,16 +13,17 @@ import LogoSelect from '../assets/logoRequire.js';
 
 const screenWidth = Dimensions.get('window').width;
 
+const styles = StyleSheet.create({
+graph: {
+    width: screenWidth,
+    height: 0.781*screenWidth,
+    resizeMode: 'contain'
+  }
+});
+
 export default function StockGraphVisual(params) {
   let stockImage = require('../assets/graphs(delete)/StockPriceGraph.png'); //replace with api call and calculated graphs!
   let stockName = params.stockName; //for posterity
   
-  return (
-      <View style={{ width: screenWidth}}>
-          <Image
-            source={stockImage}
-            style={styles.image}
-          />
-      </View>
-    );
+  return (<View style={{ width: screenWidth}}><Image source={stockImage} style={styles.graph}/></View>);
   }
