ca52a3505f93bda38c61a0ba960454910028f242
cammillen <cameron.millen.22@ucl.ac.uk>
2023-03-09 18:57:52 +0000

    changes

--- src/screens/Search.js
+++ src/screens/Search.js
@@ -1,29 +1,26 @@
-// the back button needs to take you to the page you were on, and the search bar now needs to be functionable. 
+// PLaceholder Screen
+// Chnage everything with PLaceholder in, only 2 things to change. 
 
 import React from 'react';
-import { Text, StyleSheet, View, ScrollView } from 'react-native';
+import { View, StyleSheet } from 'react-native';
 import { StatusBar } from 'react-native';
 import { useCallback } from 'react';
 import { useFonts } from 'expo-font';
 import * as SplashScreen from 'expo-splash-screen';
 import TopMenuBar from '../components/TopMenuBar.js';
-import { NavigationContainer } from '@react-navigation/native';
 import Background from '../components/Background.js';
 import BottomMenuBar from '../components/BottomMenuBar.js';
-import { useNavigation } from '@react-navigation/native';
-
-import LeftArrowTextHeader from '../components/Section Headers/LeftArrowTextHeader.js';
-import TopPerformers from '../components/BrowseFundsTopPerformersHeaders.js';
-import BrowseFundsHeader from '../components/BrowseFundsHeader.js';
-import BrowseFunds from '../components/BrowseFunds.js';
-import { TouchableOpacity } from 'react-native-gesture-handler';
-
 
 export default function Search () {
 
   const Screen = 'Search';
-  const navigation = useNavigation();
 
+  const [fontsLoaded] = useFonts({
+    'Urbanist-Bold': require('../assets/fonts/Urbanist-Bold.ttf'),
+    'Urbanist-SemiBold': require('../assets/fonts/Urbanist-SemiBold.ttf'),
+    'Urbanist-Medium': require('../assets/fonts/Urbanist-Medium.ttf'),
+    'Urbanist-Regular': require('../assets/fonts/Urbanist-Regular.ttf'),
+  });
 
   SplashScreen.preventAutoHideAsync(); 
 
@@ -37,41 +34,29 @@ export default function Search () {
     return null;
   }
 
-  const [fontsLoaded] = useFonts({
-    'Urbanist-Bold': require('../assets/fonts/Urbanist-Bold.ttf'),
-    'Urbanist-SemiBold': require('../assets/fonts/Urbanist-SemiBold.ttf'),
-    'Urbanist-Medium': require('../assets/fonts/Urbanist-Medium.ttf'),
-    'Urbanist-Regular': require('../assets/fonts/Urbanist-Regular.ttf'),
-  });
-
-  const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-    },
-    bottomMenuBarContainer:{
-      position: 'absolute',
-      bottom: 0,
-      justifyContent: 'center',
-      alignItems: 'center',
-      marginBottom: -5,
-      marginLeft: 5,
-    },
-  });
-
   return ( 
-    <NavigationContainer>
     <Background>
-        <StatusBar barStyle="light-content" backgroundColor="#000" />
+      <StatusBar barStyle="light-content" backgroundColor="#000" />
       <View style={styles.container} onLayout={onLayoutRootView}>
-        <TopMenuBar screen = {Screen}/>
-        
+        <TopMenuBar screen={Screen} />
         <View style={styles.bottomMenuBarContainer}>
-          <BottomMenuBar/>
+          <BottomMenuBar />
         </View>
       </View>
     </Background>
-    </NavigationContainer>
   );
 };
 
-const funds = ['Cambr Invest', 'Oxford Traders', 'UCL Fintech', 'StAnd Algo'];
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  bottomMenuBarContainer: {
+    position: 'absolute',
+    bottom: 0,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: -5,
+    marginLeft: 5,
+  },
+});
