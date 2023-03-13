dc6c4996d57a039c0cca4f88121c5a4d77d90385
Arei <Arei1997@hotmail.co.uk>
2023-03-13 22:26:49 +0000

    navigation for stock page changes

--- src/components/StockPage/MarketStatsDetails.js
+++ src/components/StockPage/MarketStatsDetails.js
@@ -1,12 +1,16 @@
 import React, { Component } from "react";
-import { StyleSheet, View, Image, Text, TouchableOpacity } from "react-native";
+import { StyleSheet, View, Image, Text, ScrollView, TouchableOpacity } from "react-native";
 import { Link, useNavigation } from "@react-navigation/native";
+import TopMenuBar from '../../components/TopMenuBar.js';
+
+import BottomMenuBar from '../../components/BottomMenuBar.js';
 
 function MarketStatsDetails(props) {
-  
-    const navigation = useNavigation();
+ const navigation = useNavigation();
+ const Screen = 'StockPage';
   return (
     <View style={styles.container}>
+      <TopMenuBar screen={Screen} />
       <View style={styles.group}>
         <View style={styles.image1Row}>
         <TouchableOpacity onPress={() => navigation.goBack(null)}>
@@ -19,92 +23,96 @@ function MarketStatsDetails(props) {
 
              
             </TouchableOpacity>
-       
-         
-   
-         
           <Text style={styles.blcbMarketStats}>BLCB Market Stats</Text>
-       </View>
-      </View>
-      <View style={styles.group8}>
-        <View style={styles.market_cap}>
-          <View style={styles.image2Row}>
-            <Image
-              source={require("../../assets/icons/BrowseFunds.png")}
-              resizeMode="contain"
-              style={styles.image2}
-            ></Image>
-            <Text style={styles.marketCap}>Market Cap</Text>
-            <Text style={styles.amount}>$12.75B</Text>
-          </View>
-        </View>
-        <View style={styles.price_earning}>
-          <View style={styles.image3Row}>
-            <Image
-              source={require("../../assets/icons/BrowseFunds.png")}
-              resizeMode="contain"
-              style={styles.image3}
-            ></Image>
-            <Text style={styles.priceEarningsRatio}>Price-Earnings Ratio</Text>
-            <Text style={styles.nA}>N/A</Text>
-          </View>
-        </View>
-        <View style={styles.divident_yield}>
-          <View style={styles.image4Row}>
-            <Image
-              source={require("../../assets/icons/BrowseFunds.png")}
-              resizeMode="contain"
-              style={styles.image4}
-            ></Image>
-            <Text style={styles.dividendYield}>Dividend Yield</Text>
-            <Text style={styles.nA2}>N/A</Text>
-          </View>
-        </View>
-        <View style={styles.average_volume}>
-          <View style={styles.image5Row}>
-            <Image
-              source={require("../../assets/icons/BrowseFunds.png")}
-              resizeMode="contain"
-              style={styles.image5}
-            ></Image>
-            <Text style={styles.averageVolume}>Average Volume</Text>
-            <Text style={styles.nA3}>2,789,786</Text>
-          </View>
-        </View>
-        <View style={styles.earning_per_share}>
-          <View style={styles.image6Row}>
-            <Image
-              source={require("../../assets/icons/BrowseFunds.png")}
-              resizeMode="contain"
-              style={styles.image6}
-            ></Image>
-            <Text style={styles.earningsPerShare}>Earnings Per Share</Text>
-            <Text style={styles.nA4}>N/A</Text>
-          </View>
-        </View>
-        <View style={styles.one_year_high}>
-          <View style={styles.image7Row}>
-            <Image
-              source={require("../../assets/icons/BrowseFunds.png")}
-              resizeMode="contain"
-              style={styles.image7}
-            ></Image>
-            <Text style={styles.earningsPerShare2}>1 Year High (52w)</Text>
-            <Text style={styles.nA5}>$289.86</Text>
-          </View>
-        </View>
-        <View style={styles.one_year_low}>
-          <View style={styles.image8Row}>
-            <Image
-              source={require("../../assets/icons/BrowseFunds.png")}
-              resizeMode="contain"
-              style={styles.image8}
-            ></Image>
-            <Text style={styles.text}>1 Year Low (52w)</Text>
-            <Text style={styles.text2}>$69.29</Text>
-          </View>
         </View>
       </View>
+      <View style={styles.scrollArea}>
+        <ScrollView
+          horizontal={false}
+          contentContainerStyle={styles.scrollArea_contentContainerStyle}
+        >
+          <View style={styles.market_cap}>
+            <View style={styles.image2Row}>
+              <Image
+                source={require("../../assets/icons/MarketStatsDetails/MarketCap.png")}
+                resizeMode="contain"
+                style={styles.image2}
+              ></Image>
+              <Text style={styles.marketCap}>Market Cap</Text>
+              <Text style={styles.amount}>$12.75B</Text>
+            </View>
+          </View>
+          <View style={styles.price_earning}>
+            <View style={styles.image3Row}>
+              <Image
+                source={require("../../assets/icons/MarketStatsDetails/MarketCap.png")}
+                resizeMode="contain"
+                style={styles.image3}
+              ></Image>
+              <Text style={styles.priceEarningsRatio}>
+                Price-Earnings Ratio
+              </Text>
+              <Text style={styles.nA}>N/A</Text>
+            </View>
+          </View>
+          <View style={styles.divident_yield}>
+            <View style={styles.image4Row}>
+              <Image
+                source={require("../../assets/icons/MarketStatsDetails/MarketCap.png")}
+                resizeMode="contain"
+                style={styles.image4}
+              ></Image>
+              <Text style={styles.dividendYield}>Dividend Yield</Text>
+              <Text style={styles.nA2}>N/A</Text>
+            </View>
+          </View>
+          <View style={styles.average_volume}>
+            <View style={styles.image5Row}>
+              <Image
+                source={require("../../assets/icons/MarketStatsDetails/MarketCap.png")}
+                resizeMode="contain"
+                style={styles.image5}
+              ></Image>
+              <Text style={styles.averageVolume}>Average Volume</Text>
+              <Text style={styles.nA3}>2,789,786</Text>
+            </View>
+          </View>
+          <View style={styles.earning_per_share}>
+            <View style={styles.image6Row}>
+              <Image
+                source={require("../../assets/icons/MarketStatsDetails/MarketCap.png")}
+                resizeMode="contain"
+                style={styles.image6}
+              ></Image>
+              <Text style={styles.earningsPerShare}>Earnings Per Share</Text>
+              <Text style={styles.nA4}>N/A</Text>
+            </View>
+          </View>
+          <View style={styles.one_year_high}>
+            <View style={styles.image7Row}>
+              <Image
+                source={require("../../assets/icons/MarketStatsDetails/MarketCap.png")}
+                resizeMode="contain"
+                style={styles.image7}
+              ></Image>
+              <Text style={styles.earningsPerShare2}>1 Year High (52w)</Text>
+              <Text style={styles.nA5}>$289.86</Text>
+            </View>
+          </View>
+          <View style={styles.one_year_low}>
+            <View style={styles.image8Row}>
+              <Image
+                source={require("../../assets/icons/MarketStatsDetails/MarketCap.png")}
+                resizeMode="contain"
+                style={styles.image8}
+              ></Image>
+              <Text style={styles.text}>1 Year Low (52w)</Text>
+              <Text style={styles.text2}>$69.29</Text>
+            </View>
+          </View>
+        </ScrollView>
+      </View>
+      <BottomMenuBar />
     </View>
   );
 }
@@ -142,11 +150,15 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     flex: 1
   },
-  group8: {
+  scrollArea: {
     width: 341,
-    height: 609,
+    height: 449,
     marginTop: 27,
     marginLeft: 17
+  },
+  scrollArea_contentContainerStyle: {
+    height: 609,
+    width: 341
   },
   market_cap: {
     width: 341,
