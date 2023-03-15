9705c7baef10a0812abb75683b5eb857ba4cf22a
cammillen <cameron.millen.22@ucl.ac.uk>
2023-03-15 16:35:16 +0000

    cache fixed

--- App.js
+++ App.js
@@ -1,13 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import MainDrawerNavigator from './src/navigation/MainDrawerNavigator.js';
+import AppLoading from 'expo-app-loading';
+import { Asset } from 'expo-asset';
 
 const App = () => {
+  const [isReady, setIsReady] = useState(false);
+
+  if (!isReady) {
+    return (
+      <AppLoading
+        startAsync={_cacheResourcesAsync}
+        onFinish={() => setIsReady(true)}
+        onError={console.warn}
+      />
+    );
+  }
+
   return (
     <NavigationContainer>
       <MainDrawerNavigator />
     </NavigationContainer>
   );
 };
+
+async function _cacheResourcesAsync() {
+  const images = [
+    require('./src/assets/AndroidBackground.png'),
+    require('./src/assets/IOSBackground.png'),
+  ];
+
+  const cacheImages = images.map((image) => {
+    return Asset.fromModule(image).downloadAsync();
+  });
+
+  await Promise.all(cacheImages);
+}
 
 export default App;
