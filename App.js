9da613c84983d9b561156bb4bf73623030d4a205
cammillen <cameron.millen.22@ucl.ac.uk>
2023-03-15 17:17:13 +0000

    splash screen

--- App.js
+++ App.js
@@ -3,9 +3,12 @@ import { NavigationContainer } from '@react-navigation/native';
 import MainDrawerNavigator from './src/navigation/MainDrawerNavigator.js';
 import AppLoading from 'expo-app-loading';
 import { Asset } from 'expo-asset';
+import * as Font from 'expo-font';
+import SplashScreen from './src/components/SplashScreen'; // Make sure to import the SplashScreen component
 
 const App = () => {
   const [isReady, setIsReady] = useState(false);
+  const [showSplash, setShowSplash] = useState(true);
 
   if (!isReady) {
     return (
@@ -15,6 +18,10 @@ const App = () => {
         onError={console.warn}
       />
     );
+  }
+
+  if (showSplash) {
+    return <SplashScreen onReady={() => setShowSplash(false)} />;
   }
 
   return (
@@ -28,13 +35,21 @@ async function _cacheResourcesAsync() {
   const images = [
     require('./src/assets/AndroidBackground.png'),
     require('./src/assets/IOSBackground.png'),
+    // Add any other image assets used in the Home screen
   ];
 
   const cacheImages = images.map((image) => {
     return Asset.fromModule(image).downloadAsync();
   });
 
-  await Promise.all(cacheImages);
+  const cacheFonts = Font.loadAsync({
+    'Urbanist-Bold': require('./src/assets/fonts/Urbanist-Bold.ttf'),
+    'Urbanist-SemiBold': require('./src/assets/fonts/Urbanist-SemiBold.ttf'),
+    'Urbanist-Medium': require('./src/assets/fonts/Urbanist-Medium.ttf'),
+    'Urbanist-Regular': require('./src/assets/fonts/Urbanist-Regular.ttf'),
+  });
+
+  await Promise.all([...cacheImages, cacheFonts]);
 }
 
 export default App;
